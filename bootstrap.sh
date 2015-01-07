84b23b54f33a31a5771496a9a1ef84d597eb1cc8
Sylph Lin <sylph.lin@gmail.com>
2015-01-07 19:59:34 +0800

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Bootstrap script for CentOS 7
+
+USER=sylph
+SERVER=bs
+
+adduser $USER
+usermod -a -G wheel $USER
+
+# Disable SELinux
+setenforce 0
+sed -i 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+
+# Enable Password Authentication
+sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+systemctl reload sshd.service
+
+
+# Setting VIM
+yum -y install vim
+echo -e '\ncolorscheme elflord\nset tabstop=4\nset tabstop=3\nset expandtab\nset encoding=utf-8' >> /etc/vimrc
+
+# Modify server name
+hostname $SERVER
+echo "HOSTNAME=$SERVER" >> /etc/sysconfig/network
