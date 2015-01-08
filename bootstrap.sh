1ae53739e612f5431c2c2bf9410516475cac59ab
Sylph Lin <sylph.lin@gmail.com>
2015-01-08 18:40:58 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Bootstrap script for CentOS 7
 
-USER=sylph
-SERVER=bs
+USER=user
+SERVER=server
 
 adduser $USER
 usermod -a -G wheel $USER
@@ -15,11 +15,12 @@ sed -i 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl reload sshd.service
 
+# Modify server name
+hostname $SERVER
+echo "HOSTNAME=$SERVER" >> /etc/sysconfig/network
 
 # Setting VIM
 yum -y install vim
 echo -e '\ncolorscheme elflord\nset tabstop=4\nset tabstop=3\nset expandtab\nset encoding=utf-8' >> /etc/vimrc
 
-# Modify server name
-hostname $SERVER
-echo "HOSTNAME=$SERVER" >> /etc/sysconfig/network
+
